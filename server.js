daca46974ab7bdfc81209544af2a4fec8363dd6c
Ivey Topaz <iveytopaz@gmail.com>
2016-10-17 14:44:23 -0400

    adding api fetching functions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var engines = require('consolidate');
+var fetch = require('./lib/fetchForecast.js')
 
 const app = express();
 app.set('views', __dirname + '/views');
@@ -7,4 +8,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => res.render('index.html'))
+
+app.get('/forecast', fetch.request)
+
 app.listen(3030);
