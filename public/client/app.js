e2df7544072a52845ec2aeecc9a6e96c1db20a89
Ivey Topaz <iveytopaz@gmail.com>
2016-10-17 19:38:59 -0400

    creating temperature table

--- public/client/app.js
+++ public/client/app.js
@@ -1,4 +1,6 @@
 'use strict';
+const dayMap = 'Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday'.split(',');
+
 angular.module('weather', [])
 
 .controller('weatherCtrl', function($scope, GetWeather) {
@@ -12,7 +14,7 @@ angular.module('weather', [])
 })
 
 .factory('GetWeather', function($http) {
-  var fetch = function (zip) {
+  let fetch = function (zip) {
     console.log('about to make get request with ', zip)
     return $http({
       method: 'GET',
@@ -32,8 +34,29 @@ angular.module('weather', [])
     restrict: 'E',
     link: function (scope, element, attrs) {
       //watching for new data, create visualization with new data
-      scope.$watch('data', function(newV) {
-        console.log(newV)
+      scope.$watch('data', function(data) {
+
+        if (data.weather) {
+          let visual = d3.select(element[0])
+          //make sure display is clear
+          visual.selectAll('*').remove();
+          //get just the temp and time data
+          let tempData = data.weather.map(hour => {
+            let day = new Date(hour.time*1000).getDay();
+            return {
+              temp: hour.apparentTemperature,
+              day: dayMap[day],
+              hour: new Date(hour.time*1000).getHours()
+            }
+          })
+          console.log(tempData)
+          //create a new visualizing space
+          visual.append('svg')
+            .attr('width', 650)
+            .attr('height', 400)
+            .style('background', 'lime')
+            .selectAll()
+        }
       }, true)
     }
   }
