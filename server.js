018bd3f5524cf7f9af6b21a83e8db229381b374b
Ivey Topaz <iveytopaz@gmail.com>
2016-10-17 16:14:48 -0400

    set up URL parsing for location search

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
-var engines = require('consolidate');
-var fetch = require('./lib/fetchForecast.js')
+const engines = require('consolidate');
+const fetch = require('./lib/fetchForecast.js')
+const bodyParser = require('body-parser');
 
 const app = express();
+
+//Set view engine
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+//define middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'))
+
+//handle routes
 app.get('/', (req, res) => res.render('index.html'))
 
 app.get('/forecast', fetch.request)
